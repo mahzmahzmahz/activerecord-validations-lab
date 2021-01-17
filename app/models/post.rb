6507c14408c96469dd92a3f30a6de4620ed18d2d
Alex Marz <alexhmarz@gmail.com>
2021-01-17 14:15:47 -0500

    Scorpion Bowl

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { in: 1..250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+
+    validate :clickbait
+
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
+  
+    def clickbait
+      if CLICKBAIT_PATTERNS.none? {|pattern| pattern.match(self.title)}
+        self.errors.add(:title, "is not clickbait enough")
+      end
+    end
+  end
